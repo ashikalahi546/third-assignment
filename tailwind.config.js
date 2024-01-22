105961bfd126fbc4d0263ae96e1f9d6709868431
Md.Ashik <ashikalahi546@gmail.com>
2024-01-23 01:31:58 +0600

    half of the projects are complete

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+      content: ["./src/**/*.{html,js}"],
+      theme: {
+        extend: {
+          colors:{
+            primaryRed: "#FF4240",
+            secondaryBlack:"#091318"
+          }
+        },
+      },
+      plugins: [],
+    }
