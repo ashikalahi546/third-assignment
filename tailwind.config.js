506990ec7cdf6693a4c9fc2d1734f21abd16ddb1
Md.Ashik <ashikalahi546@gmail.com>
2024-01-24 00:02:55 +0600

    Full Projects Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,7 @@ module.exports = {
       theme: {
         extend: {
           colors:{
-            primaryRed: "#FF4240",
-          
+            // primaryRed: "#FF4240",
           }
         },
       },
